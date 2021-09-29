3daf817ceab4d81c869fc45581985a5f77534922
pedro miguel <pdemeu@outlook.com>
2021-09-29 13:18:31 -0300

    Definindo Urls

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
+from core.views import IndexView
 from django.urls import path
 
 urlpatterns = [
-    
+    path('', IndexView.as_view(), name="index"),
+    #path('', IndexView.as_view(), name="index"),
+    #path('', IndexView.as_view(), name="index"),
 ]
