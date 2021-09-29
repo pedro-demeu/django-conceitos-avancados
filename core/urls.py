58da830e44bd7ce70e5fd6e5301bb0080ae3f52d
pedro miguel <pdemeu@outlook.com>
2021-09-29 17:43:12 -0300

    Boas práticas com block em Django

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
-from core.views import IndexView
+from core.views import Error404View, IndexView
 from django.urls import path
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    #path('', IndexView.as_view(), name="index"),
+    path('teste', Error404View.as_view(), name="error404"),
     #path('', IndexView.as_view(), name="index"),
 ]
