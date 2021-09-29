3c8711962afd26ab9c7e4251801ed7670c39f916
pedro miguel <pdemeu@outlook.com>
2021-09-29 19:06:01 -0300

    Criando meus Modelos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,63 @@
+import uuid
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4}.{ext}'
+    return filename
+
+class Base(models.Model):
+    criados = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Modificado', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cob', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+
+    servico = models.CharField('Serviço', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Icone', max_length=12, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self) -> str:
+        return self.servico
+
+class Cargo(Base):
+    cargo = models.CharField("Cargo", max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self) -> str:
+        return self.cargo
+        
+class Funcionario(Base):
+    cargo = models.ForeignKey("core.Cargo", on_delete=models.CASCADE)
+    nome = models.CharField("Nome", max_length=150)
+    imagem = StdImageField("Imagem", upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, "crop": True}})
+    biografia = models.TextField(max_length=200)
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+    
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+
+    def __str__(self) -> str:
+        return self.nome
